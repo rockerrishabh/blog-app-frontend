bcbbdee273deddebcd54fe1a6ebb8ef5e4da7a0d
Rishabh Kumar <rishabh.6542@gmail.com>
2024-08-29 14:29:11 +0530

    sixth commit

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,26 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const [data, setData] = useState<string>();
+  const fetchFirst = async () => {
+    const res = await fetch("https://api.rishabhportfolio.site");
+    const data: string = (await res.json()) as string;
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchFirst();
+  }, []);
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      {data && <p>{data}</p>}
     </div>
   );
 }
