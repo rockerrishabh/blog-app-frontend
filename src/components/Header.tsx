9e4969676da0eff8f759731543baa109abdb5671
Rishabh Kumar <rishabh.6542@gmail.com>
2024-08-30 15:41:19 +0530

    fixed pwa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,19 @@
+import { Link } from "@tanstack/react-router";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link to="/">
+        <img
+          className="h-10 w-40"
+          alt="Logo"
+          height={32}
+          width={32}
+          src="/images/Logo.svg"
+        />
+      </Link>
+    </header>
+  );
 }
 
 export default Header;
