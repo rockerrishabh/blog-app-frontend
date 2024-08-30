9e44d8af2fee06a76a1c694b6efdbbcf792a4e75
Rishabh Kumar <rishabh.6542@gmail.com>
2024-08-30 13:09:54 +0530

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,59 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "images/favicon.ico",
+        "images/apple-touch-icon.png",
+        "images/pwa-192x192.png",
+        "images/pwa-512x512.png",
+        "images/pwa-maskable-192x192.png",
+        "images/pwa-maskable-512x512.png",
+      ],
+      manifest: {
+        name: "Blog App",
+        short_name: "Blog",
+        icons: [
+          {
+            src: "/images/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/images/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/images/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/images/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#FFFFFF",
+        theme_color: "#FFFFFF",
+        description: "Simple Blog Application.",
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
